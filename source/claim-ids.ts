5d6a288920bfe1c686d0b6a4d267c9ae905579dd
Edgar Toll <edjopato@gmail.com>
2019-07-13 15:39:35 +0200

    docs(claim-ids): describe to grouping

--- source/claim-ids.ts
+++ source/claim-ids.ts
@@ -3,16 +3,19 @@
 import WikidataEntityStore from 'wikidata-entity-store';
 
 export const TEXT_INTEREST = [
+	// labels
 	'P1705', // native label
 	'P1448', // official name
 	'P487', // unicode character
 
+	// whats it
 	'P31', // instance of
 	'P279', // subclass of
 	'P136', // genre
 	'P361', // part of
 	'P366', // use
 
+	// meta data
 	'P21', // gender
 	'P571', // inception
 	'P577', // publication date
@@ -22,6 +25,7 @@ export const TEXT_INTEREST = [
 	'P57', // director
 	'P161', // cast
 
+	// physical quantities
 	'P131', // located in administrative entity
 	'P6375', // street adress
 	'P38', // currency
@@ -30,12 +34,14 @@ export const TEXT_INTEREST = [
 	'P2067', // mass
 	'P2048', // height
 	'P2049', // width
-	'P4330', // contains
 
+	// whats in it
+	'P4330', // contains
 	'P527', // has part
 	'P2670', // has parts of the class
 	'P186', // material used
 
+	// also interesting
 	'P460', // said to be the same as
 	'P1659' // see also
 ];
